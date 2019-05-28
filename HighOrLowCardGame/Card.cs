2361acd7e61ba4fed0ecedbf67fe9ee37edb3090
NRunks <renardo@renardoedgecombe.com>
2019-05-28 05:51:30 -0400

    Release

---
+++ HighOrLowCardGame/Card.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighOrLowCardGame
+{
+    public enum Suit
+    {
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+
+    /**
+     * Models a Card 
+     * */
+    public class Card
+    {
+        public string DisplayName { get; set; }
+        public Suit Suit { get; set; }
+        public int Value { get; set; }
+    }
+}
+
